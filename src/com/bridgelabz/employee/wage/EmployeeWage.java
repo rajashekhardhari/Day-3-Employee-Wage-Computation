5adb1dc63c95843e6325d9991db0a8262f4762bd
srajashekhar <udayarajashekhar@gmail.com>
2022-05-23 12:04:19 +0530

    Added: part time wage logic

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -6,21 +6,25 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-      
-		Random random= new Random();
-		int attendance=random.nextInt(2);
-		
-		int wagePerHour =20;
-		int fullDayHour =8;
-		int wagePerDay =0;
-		
-		if(attendance == 1) {
-			System.out.println("Employee Present..");
-		    wagePerDay=wagePerHour*fullDayHour;    
-		}else
-			System.out.println("Employee Absent..");
-		
-		System.out.println("Employee Wage Per Day :-"+wagePerDay);
-	 }
+
+		Random random = new Random();
+		int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
+
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int partTimeHour = 4;
+		int wagePerDay = 0;
+
+		if (attendance == 2) {
+			System.out.println("Employee Full Day Present..");
+			wagePerDay = wagePerHour * fullDayHour;
+		} else if (attendance == 1) {
+			System.out.println("Employee Part Time Present");
+			wagePerDay = wagePerHour * partTimeHour;
+		} else
+			System.out.println("Employee Absent");
+
+		System.out.println("Employee Wage Per Day :-" + wagePerDay);
+	}
 
 }
