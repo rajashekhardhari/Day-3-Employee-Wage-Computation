16c93c86c19501534c37f3ff4988837372a73a4b
srajashekhar <udayarajashekhar@gmail.com>
2022-05-23 15:11:25 +0530

    Added: Calculate wage till total working hour or days is reached for a month

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -14,12 +14,15 @@ public class EmployeeWage {
 		int partTimeHour = 4;
 		int wagePerDay = 0;
 		int monthlySalary = 0;
+		int monthlyHour = 0;
+		int days = 0;
 
-		for (int day = 1; day <= 20; day++) {
+		while (monthlyHour != 100 || days != 20) {
+
+			days++;
 
 			int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
-			System.out.println("randomNumber"+attendance);
-			wagePerDay =0;
+			wagePerDay = 0;
 			switch (attendance) {
 
 			case 0:
@@ -29,16 +32,19 @@ public class EmployeeWage {
 			case 1:
 				System.out.println("Employee Part Time Present");
 				wagePerDay = partTimeHour * wagePerHour;
+				monthlyHour = monthlyHour + partTimeHour;
 				break;
 			default:
 				System.out.println("Employee Full Day Present..");
 				wagePerDay = wagePerHour * fullDayHour;
+				monthlyHour = monthlyHour + fullDayHour;
 				break;
 			}
 
 			monthlySalary = monthlySalary + wagePerDay;
-			System.out.println(day + " : " + monthlySalary);
+			System.out
+					.println("Day: " + days + " :MonthlyHours: " + monthlyHour + ": Monthly Salary: " + monthlySalary);
 		}
-	}
 
+	}
 }
