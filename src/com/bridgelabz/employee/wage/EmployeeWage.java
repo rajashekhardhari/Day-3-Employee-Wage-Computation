3a3252c421d96ea31757cfb0a1aaf12bfd6620cb
srajashekhar <udayarajashekhar@gmail.com>
2022-05-23 11:44:02 +0530

    Added: Logic to check attendace

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.employee.wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-
-	}
+      
+		Random random= new Random();
+		int attendance=random.nextInt(2);
+		
+		if(attendance == 1)
+			System.out.println("Employee Present..");
+		else
+			System.out.println("Employee Absent..");
+	 }
 
 }
